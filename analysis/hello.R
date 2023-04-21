af0c3ca0cdad70be70bd9bf114e7796cb531c24f
kayinleung <ka.yin.leung@rivm.nl>
2023-04-21 10:03:59 +0200

    put libraries in script to update renv

--- analysis/hello.R
+++ analysis/hello.R
@@ -1,1 +1,17 @@
+library(readr)
+library(tidyr)
+library(ggplot2)
+
+
+data <- read_csv("gapminder.csv") %>%
+  pivot_longer(-country,
+               names_to = "year",
+               values_to = "gdpPercap",
+               names_prefix = "gdpPercap_",
+               names_transform = list(year = as.integer))
+
+data[data$country == "Australia", ] %>%
+  ggplot(aes(x = year, y = gdpPercap)) +
+  geom_line()
+
 print("Hello World")
